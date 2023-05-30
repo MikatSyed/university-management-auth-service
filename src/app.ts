9eb2defb5091d821f0b0a9b25c2046054d89c27f
MikatSyed <syednew5000@gmail.com>
2023-05-30 15:43:49 +0600

    Setup eslint prettier husky lint-stages

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ const app: Application = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: any) => {
   res.send('Working Sucessfully!')
 })
 
