734ef16c2c2605235acdcb07480b846b73df6b1c
unknown <syednew5000@gmail.com>
2023-06-17 20:33:06 +0600

    deploy setup

---
+++ dist/constants/pagination.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.academicSemesterFilterableFields = exports.paginationFields = void 0;
+exports.paginationFields = ['page', 'limit', 'sortBy', 'sortOrder'];
+exports.academicSemesterFilterableFields = [
+    'searchTerm',
+    'title',
+    'code',
+    'year',
+];
